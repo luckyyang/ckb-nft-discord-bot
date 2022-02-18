ff712c8dd9593d7a89f5f67f1c4f013e1d17641e
HA <>
2022-02-18 14:24:45 +0800

    refactor

--- src/utils/registerCommands.ts
+++ src/utils/registerCommands.ts
@@ -3,43 +3,26 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
-console.log('process.env.BOT_MANAGER_ROLE: ', process.env.BOT_MANAGER_ROLE)
+console.log('BOT_MANAGER_ROLE: ', process.env.BOT_MANAGER_ROLE)
 export const registerCommands = async (guild: Guild) => {
   // Check if an existing lunar commander role exists in the server
-  const existingBotRole = guild.roles.cache.find(
+  const existingManagerRole = guild.roles.cache.find(
     (role) => role.name === process.env.BOT_MANAGER_ROLE
   );
 
   // If no such role exists, create it
-  const botRole = existingBotRole
-    ? existingBotRole
+  const managerRole = existingManagerRole
+    ? existingManagerRole
     : await guild.roles.create({
         name: process.env.BOT_MANAGER_ROLE,
         color: "BLUE",
         reason: "Managing the guild assistant.",
       });
 
-  console.log('botRole: ', botRole)
-  // // get the list of new commands
-  // const newCommands = commandFiles.map((file) => {
-  //   const commandFilePath = path.resolve(__dirname, `../commands/${file}`);
-  //   const command = require(commandFilePath).default;
-  //   return command.data.toJSON();
-  // });
-
-  // // register the commands
-  // const rest = new REST({ version: "9" }).setToken(token);
-
-  // await rest.put(Routes.applicationGuildCommands(clientId, guild.id) as any, {
-  //   body: newCommands,
-  // });
-
-  // console.log(`Successfully registered application commands for ${guild.name}`);
-
-  // now update the permissions to allow for BOT_MANAGER_ROLE to configure the bot
+  // now update the permissions to allow for BOT_MANAGER_ROLE to use specific commands
   const permissions = [
     {
-      id: botRole.id,
+      id: managerRole.id,
       type: "ROLE" as const,
       permission: true,
     },
@@ -51,7 +34,6 @@ export const registerCommands = async (guild: Guild) => {
   const configureCommand = registeredCommands.find((command) => {
     return command.name == "guild-setup";
   });
-  console.log('configureCommand: ', configureCommand)
 
   // add command permission
   await configureCommand!.permissions.add({ permissions });
